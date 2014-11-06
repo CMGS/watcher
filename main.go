07cdb3b4084561f4a12dce1f94d3b6f328db54a3
CMGS <ilskdw@gmail.com>
2014-11-06 11:38:52 +0800

    finish watcher

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"flag"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/fsouza/go-dockerclient"
 
 	"./common"
 	"./defines"
+	"./logs"
 	"./metrics"
 )
 
@@ -25,4 +32,27 @@ func main() {
 	Metrics = metrics.NewMetricsRecorder(hostname, config)
 	common.Docker = defines.NewDocker(endpoint)
 
+	containers, err := common.Docker.ListContainers(docker.ListContainersOptions{All: true})
+	if err != nil {
+		logs.Assert(err, "Load")
+	}
+
+	logs.Info("Load container")
+	for _, container := range containers {
+		sid := container.ID[:12]
+		if strings.HasPrefix(container.Status, "Exit") {
+			logs.Info(container.Names, sid, "die")
+			continue
+		}
+		name := strings.Split(container.Names[0], "_")[0]
+		Metrics.Add(name, sid, common.DEFAULT_TYPE)
+	}
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, os.Interrupt)
+	signal.Notify(sc, syscall.SIGTERM)
+	signal.Notify(sc, syscall.SIGHUP)
+	signal.Notify(sc, syscall.SIGKILL)
+	signal.Notify(sc, syscall.SIGQUIT)
+	logs.Info("Got <-", <-sc)
 }
