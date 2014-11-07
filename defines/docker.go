2d98a19edfeb2bb6b0a17a4bc2b09e7180b76777
CMGS <ilskdw@gmail.com>
2014-11-07 11:13:54 +0800

    add mock func, make test pass

--- defines/docker.go
+++ defines/docker.go
@@ -1,6 +1,8 @@
 package defines
 
 import (
+	"reflect"
+
 	"../logs"
 	"../utils"
 	"github.com/fsouza/go-dockerclient"
@@ -34,4 +36,39 @@ func NewDocker(endpoint string) *DockerWrapper {
 	var makeDockerWrapper func(*DockerWrapper, *docker.Client) *DockerWrapper
 	utils.MakeWrapper(&makeDockerWrapper)
 	return makeDockerWrapper(d, client)
+}
+
+func MockDocker(d *DockerWrapper) {
+	var makeMockedDockerWrapper func(*DockerWrapper, *docker.Client) *DockerWrapper
+	MakeMockedWrapper(&makeMockedDockerWrapper)
+	makeMockedDockerWrapper(d, d.Client)
+}
+
+func MakeMockedWrapper(fptr interface{}) {
+	var maker = func(in []reflect.Value) []reflect.Value {
+		wrapper := in[0].Elem()
+		client := in[1]
+		wrapperType := wrapper.Type()
+		for i := 1; i < wrapperType.NumField(); i++ {
+			field := wrapper.Field(i)
+			fd, ok := client.Type().MethodByName(wrapperType.Field(i).Name)
+			if !ok {
+				logs.Info("Reflect Failed")
+				continue
+			}
+			fdt := fd.Type
+			f := reflect.MakeFunc(field.Type(), func(in []reflect.Value) []reflect.Value {
+				ret := make([]reflect.Value, 0, fdt.NumOut())
+				for i := 0; i < fdt.NumOut(); i++ {
+					ret = append(ret, reflect.Zero(fdt.Out(i)))
+				}
+				return ret
+			})
+			field.Set(f)
+		}
+		return []reflect.Value{in[0]}
+	}
+	fn := reflect.ValueOf(fptr).Elem()
+	v := reflect.MakeFunc(fn.Type(), maker)
+	fn.Set(v)
 }
